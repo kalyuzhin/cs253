5c55237117d2a2865da40864b16776455886aad2
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2025-09-14 01:16:41 +0300

    [IntSys] – implement 2nd subtask

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <ios>
-#include <unordered_map>
+#include <queue>
 #include <vector>
+#include <chrono>
 #include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
-using ll = long long;
-using ull = unsigned long long;
-using vll = vector<ll>;
-using vull = vector<ull>;
+#define endln "\n"
+#define HASH_SIZE 100000
+#define MICROSECONDS_TO_SECONDS 1000000.0
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define vull vector<ull>
 
 template<typename T>
 using vec = vector<T>;
 
+
 // Даны два целых числа – например, 2 и 100, а также две операции – «прибавить 3» и «умножить на 2».
 // Найти минимальную последовательность операций, позволяющую получить из первого числа второе.
-void subTask1() {
-    ll a, b, res;
-    cin >> a >> b;
+void subTask1(ll a, ll b) {
+    ll res;
     if (a > b) {
         cout << "Невозможно получить из первого числа второе" << endl;
     }
@@ -27,56 +32,101 @@ void subTask1() {
     for (;;) {
         if (cur % 2 == 0 && (cur / 2 < cur - 3 || cur - 3 < a)) {
             cur /= 2;
-            ++res;
         } else {
             cur -= 3;
-            ++res;
         }
+        ++res;
         if (cur == a || cur <= 0) {
             break;
         }
     }
 
-    cout << res << endl;
+    cout << res << endln;
 }
 
 // То же самое, что и в пункте 1, однако добавляется операция «вычесть 2».
-void subTask2() {
-    ll a, b, res;
-    cin >> a >> b;
+void subTask2(ll a, ll b) {
+    ll res, cntVerts, tmpVal, tmpCnt, cnt;
     if (a > b) {
         cout << "Невозможно получить из первого числа второе" << endl;
     }
-    res = 0;
-    ll cur = a;
-    unordered_map<ll, ll> attended;
-    attended.reserve(10000);
 
-    for (;;) {
+    res = 0, cntVerts = 0, cnt = 0;
+    queue<pair<ll, ll>> q;
+    q.push(make_pair(a, 0));
 
-        if (cur == b) {
+    unordered_set<ll> visited;
+    visited.reserve(HASH_SIZE);
+    visited.insert(a);
+
+
+    for (; !q.empty();) {
+        tmpVal = q.front().first, tmpCnt = q.front().second;
+        q.pop();
+        ++cntVerts;
+        if (tmpVal == b) {
+            res = tmpCnt;
             break;
         }
+        for (ll i = 0; i != 3; ++i) {
+            ll nextVal = tmpVal;
+            cnt = tmpCnt;
+            if (i == 0) {
+                nextVal += 3;
+                ++cnt;
+            } else if (i == 1) {
+                nextVal *= 2;
+                ++cnt;
+            } else {
+                nextVal -= 2;
+                ++cnt;
+            }
+
+            if (nextVal < 0 && b >= 0) {
+                continue;
+            }
+            if (nextVal > b * 2 && b > 0) {
+                continue;
+            }
+
+            if (visited.find(nextVal) == visited.end()) {
+                visited.insert(nextVal);
+                q.push(make_pair(nextVal, cnt));
+            }
+        }
+
+
     }
 
-    for
+    cout << "Кол-во посещенных вершин: " << cntVerts << endln;
+    cout << "Мин кол-во шагов: " << res << endln;
 }
 
 //Реализовать задание из пункта 1 методом обратного поиска – от целевого состояния к начальному.
 // Сравнить эффективность.
-void subTask3() {
+void subTask3(ll a, ll b) {
 
 }
 
 // Дополнительное задание. Реализовать метод двунаправленного поиска для решения задачи из пункта 1.
-void subTask4() {
+void subTask4(ll a, ll b) {
 
+}
+
+void countTime(ll a, ll b, void f(ll a, ll b)) {
+    chrono::time_point start = chrono::high_resolution_clock::now();
+    f(a, b);
+    chrono::time_point end = chrono::high_resolution_clock::now();
+    chrono::microseconds microseconds = chrono::duration_cast<chrono::microseconds>(end - start);
+    double seconds = microseconds.count() / MICROSECONDS_TO_SECONDS;
+    cout << "Время: " << seconds << " секунд" << endl;
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    subTask1();
+    ll a = 2, b = 10000001;
+    countTime(a, b, subTask2);
 
     return 0;
 }
